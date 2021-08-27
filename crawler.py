4634e9648318d790544effc836713c6ede948079
Parsa Hosseini <parsahosseini2001@gmail.com>
2021-08-27 22:18:08 +0430

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,12 +6,13 @@ import ssl
 import re
 from random import randint
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
 
 def makeSoup(letterNum):
+    # Ignore SSL certificate errors
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
     url = f'https://en.wikisource.org/wiki/Moral_letters_to_Lucilius/Letter_{letterNum}'
     html = urllib.request.urlopen(url, context=ctx).read()
     return BeautifulSoup(html, 'html.parser')
@@ -25,13 +26,7 @@ def retrieveLetter(letterNum):
     return letter + f'\n&mdash; <cite>Lucius Annaeus Seneca, <em>Moral Letters to Lucilius</em>, {letterNum}</cite>'
         
 def retrieveRandomLetter():
-    letterNum = randint(1, 124)
-    letter = ''
-    soup = makeSoup(letterNum)
-    paragraphs = soup.find(id='mw-content-text').find_all('p')
-    for paragraph in paragraphs:
-        letter += paragraph.text
-    return letter + f'\n&mdash; <cite>Lucius Annaeus Seneca, <em>Moral Letters to Lucilius</em>, {letterNum}</cite>'
+    return retrieveLetter(randint(1, 124))
         
 def retrieveSectionBySectionNumber(letterNum, sectionNum):
     letter = ''
@@ -49,6 +44,6 @@ if __name__ == '__main__':
     if letterNum == 'random':
         print(retrieveRandomLetter())
     else:
-        print(retrieveLetter(letterNum));
+        print(retrieveLetter(letterNum))
         sectionNumber = input('Enter the section number: ')
         print(retrieveSectionBySectionNumber(letterNum, sectionNumber))
