c89ab4d31f4f9550e4bb6c4d9e5f852d7c9846a9
Parsa Hosseini <parsahosseini2001@gmail.com>
2021-08-27 19:13:59 +0430

    Add retrieveRandomLetter function

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 import re
+from random import randint
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -23,6 +24,15 @@ def retrieveLetter(letterNum):
         letter += paragraph.text
     return letter + f'\n&mdash; <cite>Lucius Annaeus Seneca, <em>Moral Letters to Lucilius</em>, {letterNum}</cite>'
         
+def retrieveRandomLetter():
+    letterNum = randint(1, 125)
+    letter = ''
+    soup = makeSoup(letterNum)
+    paragraphs = soup.find(id='mw-content-text').find_all('p')
+    for paragraph in paragraphs:
+        letter += paragraph.text
+    return letter + f'\n&mdash; <cite>Lucius Annaeus Seneca, <em>Moral Letters to Lucilius</em>, {letterNum}</cite>'
+        
 def retrieveSectionBySectionNumber(letterNum, sectionNum):
     letter = ''
     soup = makeSoup(letterNum)
@@ -35,7 +45,10 @@ def retrieveSectionBySectionNumber(letterNum, sectionNum):
     return re.findall(pattern, letter)[0][1] + f'\n&mdash; <cite>Lucius Annaeus Seneca, <em>Moral Letters to Lucilius</em>, {letterNum}.{sectionNum}</cite>'
 
 if __name__ == '__main__':
-    letterNum = input('Enter the letter number: ')
-    print(retrieveLetter(letterNum));
-    sectionNumber = input('Enter the section number: ')
-    print(retrieveSectionBySectionNumber(letterNum, sectionNumber))
+    letterNum = input('Enter the letter number(or random): ')
+    if letterNum == 'random':
+        print(retrieveRandomLetter())
+    else:
+        print(retrieveLetter(letterNum));
+        sectionNumber = input('Enter the section number: ')
+        print(retrieveSectionBySectionNumber(letterNum, sectionNumber))
