9a07375304af2f97e1824ee0587d1ee133ebe751
Parsa Hosseini <parsahosseini2001@gmail.com>
2021-08-29 23:43:58 +0430

    Fix bugs

--- database.py
+++ database.py
@@ -1,11 +1,21 @@
 import sqlite3
 from random import randint
+import re
 
 def retrieveSection(con, letterNumber, letterSection):
     #con = sqlite3.connect('letters.db')
     cur = con.cursor()
     cur.execute("SELECT letter_content FROM letters WHERE letter_number=? AND letter_section=?", (letterNumber, letterSection,))
-    return cur.fetchall()[0][0]
+    return re.findall(f'{letterSection}\. ([\s\S]*)' ,cur.fetchall()[0][0])[0] + f'\n&mdash; <cite>Lucius Annaeus Seneca, <em>Moral Letters to Lucilius</em>, {letterNumber}.{letterSection}</cite>'
+
+def retrieveSectionWithNumber(con, letterNumber, letterSection):
+    #con = sqlite3.connect('letters.db')
+    cur = con.cursor()
+    cur.execute("SELECT letter_content FROM letters WHERE letter_number=?", (letterNumber,))
+    li = cur.fetchall()
+    if int(letterSection) == len(li):
+        return li[int(letterSection) - 1][0] + f'\n&mdash; <cite>Lucius Annaeus Seneca, <em>Moral Letters to Lucilius</em>, {letterNumber}</cite>'
+    return li[int(letterSection) - 1][0]
 
 def retrieveLetter(con, letterNumber):
     #con = sqlite3.connect('letters.db')
@@ -21,9 +31,9 @@ if __name__ == '__main__':
     con = sqlite3.connect('letters.db')
     letterNum = input('Enter the letter number(or random): ')
     if letterNum == 'random':
-        print(retrieveLetter(con, randint(1, 125)))
+        print(retrieveLetter(con, randint(1, 124)))
     else:
         print(retrieveLetter(con, letterNum))
         sectionNumber = input('Enter the section number: ')
-        print(retrieveSection(con, letterNum, sectionNumber))
+        print(retrieveSectionWithNumber(con, letterNum, sectionNumber))
     con.close()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sqlite3
 import discord
 import os
 import database
 from keep_alive import keep_alive
 from random import randint
+import sqlite3
 
 client = discord.Client()
 
@@ -16,6 +18,7 @@ async def on_message(message):
         return
 
     if message.content.startswith('$letter'):
+        con = sqlite3.connect('letters.db')
         li = list(message.content.split())
         if li[1] == 'random':
             letterNum = randint(1, 124)
@@ -23,15 +26,16 @@ async def on_message(message):
             letterNum = li[1]
         if len(li) == 3:
             sectionNum = li[2]
-            await message.channel.send(database.retrieveSection(letterNum, sectionNum))
+            await message.channel.send(database.retrieveSection(con, letterNum, sectionNum))
         if len(li) == 2:
             sectionNum = 1
             while True:
                 try:
-                    await message.channel.send(database.retrieveSection(letterNum, sectionNum))
+                    await message.channel.send(database.retrieveSectionWithNumber(con, letterNum, sectionNum))
                     sectionNum += 1
                 except:
                     break
+        con.close()
 
 keep_alive()
 
